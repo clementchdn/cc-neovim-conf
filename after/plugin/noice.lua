cb683f08d4ecaf14f90e942ea67c606924c3c776
Clement Chandon <clement.chandon@thalesgroup.com>
2024-02-20 10:23:50 +0100

    removes background warning

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -1,3 +1,7 @@
+require("notify").setup({
+  background_colour = "#000000"
+})
+
 require("noice").setup({
   lsp = {
     override = {
@@ -29,5 +33,15 @@ require("noice").setup({
     opts = {
       position = "50%"
     }
-  }
+  },
+  notify = {
+    -- Noice can be used as `vim.notify` so you can route any notification like other messages
+    -- Notification messages have their level and other properties set.
+    -- event is always "notify" and kind can be any log level as a string
+    -- The default routes will forward notifications to nvim-notify
+    -- Benefit of using Noice for this is the routing and consistent history view
+    enabled = true,
+    view = "notify",
+  },
 })
+
