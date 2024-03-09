25bbc23f2f14486b9bf5f7c3d5503cc1f139946e
clement <clement@Ubuntu22.myguest.virtualbox.org>
2024-03-09 14:42:30 +0100

    change leap mappings to prevent conflicts with nvim-surround

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -1,6 +1,10 @@
 local leap = require("leap")
 
-leap.create_default_mappings()
+-- leap.create_default_mappings()
+vim.keymap.set({'n', 'x', 'o'}, 'zj', '<Plug>(leap-forward)')
+vim.keymap.set({'n', 'x', 'o'}, 'zk', '<Plug>(leap-backward)')
+vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
+
 leap.opts.special_keys.prev_target = '<bs>'
 leap.opts.special_keys.prev_group = '<bs>'
 require('leap.user').set_repeat_keys('<cr>', '<bs>')
