5b824daa8ed3fea8188b963036152377fcca15b2
clement <clement.chandon@gmail.com>
2024-08-05 16:52:56 +0200

    updates keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -263,8 +263,8 @@ cmp.setup({
 		-- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
 		--
 		-- -- scroll documentation window
-		-- ['<C-f>'] = cmp.mapping.scroll_docs(-5),
-		-- ['<C-d>'] = cmp.mapping.scroll_docs(5),
+		["<C-u>"] = cmp.mapping.scroll_docs(-5),
+		["<C-d>"] = cmp.mapping.scroll_docs(5),
 
 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
